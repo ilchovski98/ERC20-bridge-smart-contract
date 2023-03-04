864659f4a99eb8a5bc8e8df5880e106219d90a7f
ilchovski98 <ilchovski98@gmail.com>
2023-03-04 16:41:11 +0200

    Improve claim and deposit functions + add new events for each case

--- contracts/BridgeFactory.sol
+++ contracts/BridgeFactory.sol
@@ -2,24 +2,54 @@
 pragma solidity 0.8.17;
 
 import "@openzeppelin/contracts/interfaces/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "./WERC20.sol";
-contract BridgeFactory {
-  event CrossTransfer(
-    address originalTokenAddress,
+contract BridgeFactory is Ownable {
+  // Deposit function
+  // Send token transfer
+  event LockOriginalToken(
+    address lockedTokenAddress,
     uint256 amount,
     address sender,
     address recepient,
-    uint256 fromChainId,
-    uint256 toChainId
+    uint256 sourceChainId,
+    uint256 destinationChainId
   );
 
-  event AcceptCrossTransfer(
-    address originalTokenAddress,
+  // Deposit function
+  // Send Wrapped token transfer
+  event BurnWrappedToken(
+    address burnedWrappedTokenAddress,
+    uint256 amount,
+    address sender,
+    address recepient,
+    uint256 sourceChainId,
+    uint256 destinationChainId,
+    address destinationTokenAddress
+  );
+
+  // Claim function
+  // Receive Wrapped token transfer
+  event ReleaseOriginalToken(
+    address releasedTokenAddress,
     uint256 amount,
     address sender,
     address recepient,
     uint256 fromChainId,
-    uint256 toChainId
+    uint256 toChainId,
+    address sourceWrappedTokenAddress
+  );
+
+  // Claim function
+  // Receive original token transfer
+  event MintWrappedToken(
+    address mintedTokenAddress,
+    uint256 amount,
+    address sender,
+    address recepient,
+    uint256 fromChainId,
+    uint256 toChainId,
+    address sourceTokenAddress
   );
 
   struct OriginalToken {
@@ -27,15 +57,17 @@ contract BridgeFactory {
 		uint256 originChainId;
 	}
 
-  mapping(bytes32 => bool) public areTokensClaimedByTransactionHash;
   mapping(uint256 => mapping(address => address)) public wrappedTokenByOriginalTokenByChainId; // chainId => original token => wrapped token address
   mapping(address => OriginalToken) public originalTokenByWrappedToken; // wrapped token => original token
 
-
   // EIP712
   bytes32 public DOMAIN_SEPARATOR;
-  // keccak256("DepositWithPermit(address owner,address spender,address token,uint256 amount,uint256 deadline,address recepientOnDestinationChain,uint256 destinationChainId, uint256 nonce)")
+  // keccak256("DepositWithPermit(address tokenOwner,address spender,address token,uint256 amount,uint256 deadline,address recepientOnDestinationChain,uint256 destinationChainId, uint256 nonce)")
   bytes32 public constant DEPOSIT_WITH_PERMIT_TYPEHASH = 0xd4abd86c44f2eaecd99d949aa27fcaee6fe75ad6bc9d7ebb6c5f2ae5568e8a62;
+
+  // keccak256("Claim(address tokenOwner,address recepient,address originalTokenAddress,string originalTokenName,string originalTokenSymbol,uint256 amount,uint256 sourceChainId,uint256 destinationChainId,bytes32 transactionHash,uint256 nonce)")
+  bytes32 public constant CLAIM_TYPEHASH = 0xd565985c36462af230a0275c7851ff732d62cb7b1b65300d64e728a97edcf33a;
+
   mapping(address => uint256) private nonces;
 
   constructor() {
@@ -48,9 +80,18 @@ contract BridgeFactory {
     ));
   }
 
+  // Deposit			Claim
+  // Lock/Burn		Release/Mint
+
+
+  // On deposit1 if no data about coin lock
+  // On claim1 if event data says erc20 is deposited create wrapped and store data about wrapped coin
+  // On deposit2 if data about coin, burn and send data that it is wrapped coin
+  // On claim2 if event data says wrapped, release the stored coins
+
   // function deposit() external {} // no permit
   function depositWithPermit(
-    address owner,
+    address tokenOwner,
     address spender,
     address token,
     uint256 amount,
@@ -70,14 +111,80 @@ contract BridgeFactory {
         DOMAIN_SEPARATOR,
         keccak256(abi.encode(
           DEPOSIT_WITH_PERMIT_TYPEHASH,
-          owner,
+          tokenOwner,
           spender,
           token,
           amount,
           deadline,
           recepientOnDestinationChain,
           destinationChainId,
-          nonces[owner]++
+          nonces[tokenOwner]++
+        ))
+      )
+    );
+
+    address recoveredAddress = ecrecover(digest, v, r, s);
+
+    require(
+      recoveredAddress != address(0) && recoveredAddress == tokenOwner,
+      "BridgeDepositWithPermit: INVALID_SIGNATURE"
+    );
+
+    // approve
+    // token != address(0) check or no need?
+    IERC20 originalToken = IERC20(token);
+    // TODO: replace approve and transferFrom with permitTransferFrom
+    originalToken.approve(spender, amount);
+    // transfer tokens
+    originalToken.transferFrom(tokenOwner, spender, amount);
+
+    // check if token is wrapped check with originalTokenByWrappedToken
+    // check if token is being sent to the correct chain with originalTokenByWrappedToken
+    bool isWrappedToken = originalTokenByWrappedToken[token].tokenAddress != address(0);
+
+    if (isWrappedToken) {
+      require(originalTokenByWrappedToken[token].originChainId == destinationChainId, "This token cannot be sent to this destination chain");
+      WERC20(token).burn(amount);
+      emit BurnWrappedToken(token, amount, tokenOwner, recepientOnDestinationChain, block.chainid, destinationChainId, originalTokenByWrappedToken[token].tokenAddress);
+    } else {
+      // Emit event
+      emit LockOriginalToken(token, amount, tokenOwner, recepientOnDestinationChain, block.chainid, destinationChainId);
+    }
+
+  }
+
+  function claim(
+    address tokenOwner,
+    address recepient,
+    address originalTokenAddress,
+    address targetTokenAddress,
+    string calldata originalTokenName, // string or bytes32?
+    string calldata originalTokenSymbol,
+    uint256 amount,
+    uint256 sourceChainId,
+    uint256 destinationChainId,
+    bool isClaimabledTokenWrapped,
+    uint8 v,
+    bytes32 r,
+    bytes32 s
+  ) external {
+    // validate signature
+    bytes32 digest = keccak256(
+      abi.encodePacked(
+        "\x19\x01",
+        DOMAIN_SEPARATOR,
+        keccak256(abi.encode(
+          CLAIM_TYPEHASH,
+          tokenOwner,
+          recepient,
+          originalTokenAddress,
+          originalTokenName,
+          originalTokenSymbol,
+          amount,
+          sourceChainId,
+          destinationChainId,
+          transactionHash,
+          nonces[tokenOwner]++
         ))
       )
     );
@@ -86,54 +193,54 @@ contract BridgeFactory {
 
     require(
       recoveredAddress != address(0) && recoveredAddress == owner,
-      "BridgeDepositWithPermit: INVALID_SIGNATURE"
+      "BridgeClaim: INVALID_SIGNATURE"
     );
 
-    // approve
-    // token != address(0) check or no need?
-    IERC20 originalToken = IERC20(token);
-    originalToken.approve(spender, amount);
+    // Deposit			Claim
+    // Lock/Burn		Release/Mint || deploy/no-deploy
 
-    // transfer tokens
-    // Risk of reentrancy?
-    originalToken.transferFrom(owner, spender, amount);
+    // On deposit1 if no data about coin lock
+    // On claim1 if event data says erc20 is deposited create wrapped and store data about wrapped coin
+    // On deposit2 if data about coin, burn and send data that it is wrapped coin
+    // On claim2 if event data says wrapped, release the stored coins
 
-    // if WERC20 tokens burn them
-    // Make this check sooner so we dont do double casting IERC20 and WERC20? Is it expensive to cast?
-    if (wrappedTokenByOriginalTokenByChainId[block.chainid][token] != address(0)) {
-      WERC20(token).burn(amount);
+    if (targetTokenAddress == address(0)) {
+      // if claimable wrapped token has data use the already deployed contract
+      // if not deploy the contract and use it
+      if (wrappedTokenByOriginalTokenByChainId[sourceChainId][originalTokenAddress] == address(0)) {
+        WERC20 newWrappedToken = new WERC20(string.concat("Wrapped ", originalTokenName), string.concat("W", originalTokenSymbol));
+        wrappedTokenByOriginalTokenByChainId[sourceChainId][originalTokenAddress] = address(newWrappedToken);
+        originalTokenByWrappedToken[address(newWrappedToken)] = OriginalToken(originalTokenAddress, fromChainId);
+      }
+      // Mint the tokens to the destination address
+      WERC20(wrappedTokenByOriginalTokenByChainId[sourceChainId][originalTokenAddress]).mint(amount, recepient);
+
+      // Emit event
+      emit MintWrappedToken(
+        wrappedTokenByOriginalTokenByChainId[sourceChainId][originalTokenAddress],
+        amount,
+        tokenOwner,
+        recepient,
+        sourceChainId,
+        destinationChainId,
+        originalTokenAddress
+      );
+    } else {
+      // Original token
+      // transfer original token to recepient
+      // the bridge needs to approve
+      IERC20(targetTokenAddress).permitTransferFrom(recepient);
+      // Emit event
+      emit ReleaseOriginalToken(
+        targetTokenAddress,
+        amount,
+        tokenOwner,
+        recepient,
+        sourceChainId,
+        destinationChainId,
+        originalTokenAddress
+      );
     }
-
-    // Emit event
-    emit CrossTransfer(token, amount, owner, recepientOnDestinationChain, block.chainid, destinationChainId);
-  }
-
-  function claim(
-    address originalTokenAddress,
-    string memory originalTokenName,
-    string memory originalTokenSymbol,
-    uint256 amount,
-    address sender,
-    address recepient,
-    uint256 sourceChainId,
-    uint256 destinationChainId,
-    bytes32 transactionHash,
-    uint8 v,
-    bytes32 r,
-    bytes32 s
-  ) external {
-
-    // validate signature
-
-    // if Wrapped erc20 doesnt exist create new WERC20 contract
-    if (wrappedTokenByOriginalTokenByChainId[fromChainId][originalTokenAddress] == address(0)) { // originalTokenByWrappedToken[newWrappedToken.address]
-      WERC20 newWrappedToken = new WERC20(string.concat("Wrapped ", originalTokenName), string.concat("W ", originalTokenSymbol));
-      originalTokenByWrappedToken[address(newWrappedToken)] = OriginalToken(originalTokenAddress, fromChainId);
-    }
-
-    // Mint the tokens to the msg.sender address
-
-    // Emit event
   }
 
   function createNewWrappedToken(address erc20) internal {
--- contracts/WERC20.sol
+++ contracts/WERC20.sol
@@ -1,8 +1,64 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity 0.8.17;
 
 import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
 
 contract WERC20 is ERC20PresetMinterPauser {
-  constructor(string memory _name, string memory _symbol) ERC20PresetMinterPauser(_name, _symbol) {}
+  mapping(address => uint256) private nonces;
+
+  // EIP712
+  bytes32 public DOMAIN_SEPARATOR;
+  // keccak256("PermitTransferFrom(address owner,address spender,uint256 amount,uint256 nonce,uint256 deadline)")
+  bytes32 public constant PERMIT_TYPEHASH = 0xdc278f4e77e6f658ea562d0e3def6941312028c19a2b807ab422e211758c69e9;
+
+  constructor(string memory _name, string memory _symbol) ERC20PresetMinterPauser(_name, _symbol) {
+    DOMAIN_SEPARATOR = keccak256(abi.encode(
+      keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
+      keccak256(bytes(_name)),
+      keccak256(bytes("1")),
+      block.chainid,
+      address(this)
+    ));
+  }
+
+  function permitTransferFrom(
+    address owner,
+    address spender,
+    uint256 amount,
+    uint256 deadline,
+    uint8 v,
+    bytes32 r,
+    bytes32 s
+  ) external {
+    require(deadline >= block.timestamp, "ERC20WithPermit: EXPIRED_SIGNATURE");
+
+    bytes32 digest = keccak256(
+      abi.encodePacked(
+        "\x19\x01",
+        DOMAIN_SEPARATOR,
+        keccak256(abi.encode(
+          PERMIT_TYPEHASH,
+          owner,
+          spender,
+          amount,
+          nonces[owner]++,
+          deadline
+        ))
+      )
+    );
+
+    address recoveredAddress = ecrecover(digest, v, r, s);
+
+    require(
+      recoveredAddress != address(0) && recoveredAddress == owner,
+      "ERC20WithPermit: INVALID_SIGNATURE"
+    );
+
+    _approve(owner, spender, amount);
+    transferFrom(owner, spender, amount);
+  }
+
+  function getNonce(address _address) external view returns(uint) {
+    return nonces[_address];
+  }
 }
